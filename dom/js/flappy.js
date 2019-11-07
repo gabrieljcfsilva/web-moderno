05fb36a46f92b4e14576390f1c9080d1417ed024
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-07 15:09:00 -0300

    criando passaro mais animação

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -59,19 +59,50 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
                 par.setX(par.getX() + espaco * this.pares.length)
                 par.sortearAbertura()
             }
-            
+
             const meio = largura / 2
-            const cruzouOMeio = par.getX() + deslocamento >= meio 
+            const cruzouOMeio = par.getX() + deslocamento >= meio
                 && par.getX() < meio
             if (cruzouOMeio) notificarPonto()
         })
     }
 }
 
+function Passaro(alturaJogo) {
+    let voando = false
+    
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = './imgs/passaro.png'
 
-// const b = new Barreiras(600,1200, 300, 400)
-// const areaDoJogo = document.querySelector('[wm-flappy]')
-// b.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-// setInterval(() => {
-//     b.animar()
-// },3)
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = () => voando = true
+    window.onkeyup = () => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        } else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        } else {
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
+
+const b = new Barreiras(600, 1200, 300, 400)
+const passaro = new Passaro(450)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+areaDoJogo.appendChild(passaro.elemento)
+b.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(() => {
+    b.animar()
+    passaro.animar()
+}, 20)
