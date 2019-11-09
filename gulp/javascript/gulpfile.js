4be7bc76df960be68b20a0c4aa9bceaae8682655
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-09 07:39:14 -0300

    minificando arquivos e transpilando codigos

---
+++ gulp/javascript/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+// const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+gulp.task('default', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(babel({
+            minified : true, //também minifica o arquivo
+            comments : false,
+            presets: ['env']
+        }))
+        // .pipe(uglify()) //minifica o arquivo
+        .on('error', err => console.log(err)) //para conseguir descobrir algum problema no processo de execução
+        .pipe(concat('codigo.min.js')) //concatenar os arquivos e enviar para um novo arquivo minificado
+        .pipe(gulp.dest('buil')) //criando pasta para enviar os arquivos
+})
