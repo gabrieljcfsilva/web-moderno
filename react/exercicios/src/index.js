a9e597bfd66087b223a967306789eb0c746ac4d2
Gabriel Nogueira <gabrieljcf@hotmail.com>
2019-11-26 18:43:51 -0300

    exportando multiplos componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
-import ComponenteParametro from './components/ComponenteParametro'
+// import ComponenteParametro from './components/ComponenteParametro'
+import Multi, { BoaTarde, BoaNoite } from './components/Multiplos'
 
-ReactDOM.render(<ComponenteParametro pessoa="Bruno" />, document.getElementById('root'))
+ReactDOM.render(
+    <>
+        <Multi.BoaTarde nome="Bruno" />
+        <BoaNoite nome="Bruno" />
+    </>
+, document.getElementById('root'))
