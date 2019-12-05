69ad3046a1048a6f9525bf8f4db0ca21b7be48a5
Bruno Gabriel <gabrieljcfsilva@gmail.com>
2019-12-05 17:52:44 -0300

    projeto calculadora

---
+++ react/calculator/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Button.css'
+
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.triple ? 'triple' : ''
+    classes += props.double ? 'double' : ''
+
+    return (
+        <button
+            className={classes}
+            onClick={() => props.click && props.click(props.label)}
+        >
+            {props.label}
+        </button>
+    )
+}
